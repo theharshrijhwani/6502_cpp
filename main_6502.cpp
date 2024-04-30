406b6947f038619eb17806d1ab73e831ce83f971
Harshvardhan Rijhwani <harshrijhwani4@gmail.com>
2024-04-30 18:15:58 +0530

    emulate JSR

--- main_6502.cpp
+++ main_6502.cpp
@@ -15,14 +15,21 @@ struct Mem {
         }
     }
 
-    // reading byte from memory
+    // reading 1 byte from memory
     Byte operator[](u32 address) const {
         return Data[address];
     }
 
-    // writing byte to memory
+    // writing 1 byte to memory
     Byte& operator[](u32 address) {
         return Data[address];
+    }
+
+    // writing 2 bytes to memory
+    void WriteWord(u32& Cycles, Word Value, u32 Address) {
+        Data[Address] = Value & 0xFF;
+        Data[Address + 1] = (Value >> 8);
+        Cycles -= 2;
     }
 };
 
@@ -62,9 +69,20 @@ struct CPU {
         return Data;
     }
 
+    Word FetchWord(u32 Cycles, Mem& memory) { 
+        Word Data = memory[PC];
+        PC++;
+        Data = (memory[PC] << 8);
+        PC++;
+        Cycles -= 2;
+        return Data;
+    }
+
+
     static constexpr Byte INS_LDA_IM = 0xA9, 
         INS_LDA_ZP = 0xA5,
-        INS_LDA_ZPX = 0xB5;
+        INS_LDA_ZPX = 0xB5,
+        INS_JSR = 0x20;
 
     void LDASetStatus() {
         Z = (A == 0);
@@ -77,26 +95,33 @@ struct CPU {
                 Cycles,
                 memory);  // fetch instruction with help of program counter
             switch (Ins) {
-            case INS_LDA_IM: {
-                Byte Value = FetchByte(Cycles, memory);
-                A = Value;
-                LDASetStatus();
-            } break;
-            case INS_LDA_ZP: {
-                Byte ZeroPageAddr = FetchByte(Cycles, memory);
-                A = ReadByte(Cycles, ZeroPageAddr, memory);
-                LDASetStatus();
-            } break;
-            case INS_LDA_ZPX: {
-                Byte ZeroPageAddr = FetchByte(Cycles, memory);
-                ZeroPageAddr += X;
-                Cycles--;
-                A = ReadByte(Cycles, ZeroPageAddr, memory);
-                LDASetStatus();
-            } break;
-            default: {
-                printf("instruction not handled %d", Ins);
-            } break;
+                case INS_LDA_IM: {
+                    Byte Value = FetchByte(Cycles, memory);
+                    A = Value;
+                    LDASetStatus();
+                } break;
+                case INS_LDA_ZP: {
+                    Byte ZeroPageAddr = FetchByte(Cycles, memory);
+                    A = ReadByte(Cycles, ZeroPageAddr, memory);
+                    LDASetStatus();
+                } break;
+                case INS_LDA_ZPX: {
+                    Byte ZeroPageAddr = FetchByte(Cycles, memory);
+                    ZeroPageAddr += X;
+                    Cycles--;
+                    A = ReadByte(Cycles, ZeroPageAddr, memory);
+                    LDASetStatus();
+                } break;
+                case INS_JSR: {
+                    Word SubAddr = FetchWord(Cycles, memory);
+                    memory.WriteWord(Cycles, PC - 1, SP);
+                    PC = SubAddr;
+                    SP++;
+                    Cycles--;
+                } break;
+                default: {
+                    printf("instruction not handled %d", Ins);
+                } break;
             }
         }
     }
@@ -107,11 +132,13 @@ int main() {
     Mem mem;
     cpu.Reset(mem);
     // start - inline program
-    mem[0xFFFC] = CPU::INS_LDA_ZP;
+    mem[0xFFFC] = CPU::INS_JSR;
     mem[0xFFFD] = 0x42;
-    mem[0x0042] = 0x15;
+    mem[0xFFFE] = 0x42;
+    mem[0x4242] = CPU::INS_LDA_IM;  
+    mem[0x4243] = 0x84;
     // end - inline program
-    cpu.Execute(3, mem);
+    cpu.Execute(9, mem);
 
     return 0;
 }
