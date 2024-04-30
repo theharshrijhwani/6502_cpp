71e0ed62302c66a53f727884f8a62fc503f2f072
Harshvardhan Rijhwani <harshrijhwani4@gmail.com>
2024-04-30 17:34:08 +0530

    emulate LDA Zero page

--- main_6502.cpp
+++ main_6502.cpp
@@ -56,7 +56,18 @@ struct CPU {
         return Data;
     }
 
-    static constexpr Byte INS_LDA_IM = 0xA9;
+    Byte ReadByte(u32 Cycles, Byte Address,Mem& memory) {
+        Byte Data = memory[Address];
+        Cycles--;
+        return Data;
+    }
+
+    static constexpr Byte INS_LDA_IM = 0xA9, INS_LDA_ZP = 0xA5;
+
+    void LDASetStatus() {
+        Z = (A == 0);
+        N = (A & 0b10000000) > 0;
+    }
 
     void Execute(u32 Cycles, Mem& memory) {
         while (Cycles--) {
@@ -67,8 +78,12 @@ struct CPU {
             case INS_LDA_IM: {
                 Byte Value = FetchByte(Cycles, memory);
                 A = Value;
-                Z = (A == 0);
-                N = (A & 0b10000000) > 0;
+                LDASetStatus();
+            } break;
+            case INS_LDA_ZP: {
+                Byte ZeroPageAddr = FetchByte(Cycles, memory);
+                A = ReadByte(Cycles, ZeroPageAddr, memory);
+                LDASetStatus();
             } break;
             default: {
                 printf("instruction not handled %d", Ins);
@@ -83,10 +98,11 @@ int main() {
     Mem mem;
     cpu.Reset(mem);
     // start - inline program
-    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFC] = CPU::INS_LDA_ZP;
     mem[0xFFFD] = 0x42;
+    mem[0x0042] = 0x15;
     // end - inline program
-    cpu.Execute(2, mem);
+    cpu.Execute(3, mem);
 
     return 0;
 }
