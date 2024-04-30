10c72fd69f8d9ab1492bc48cd8a5b7d09efc86cb
Harshvardhan Rijhwani <harshrijhwani4@gmail.com>
2024-04-30 16:14:48 +0530

    shift to visual studio

--- main_6502.cpp
+++ main_6502.cpp
@@ -19,6 +19,11 @@ struct Mem {
     Byte operator[](u32 address) const {
         return Data[address];
     }
+
+    // writing byte to memory
+    Byte& operator[](u32 address) {
+        return Data[address];
+    }
 };
 
 struct CPU {
@@ -35,7 +40,7 @@ struct CPU {
     Byte V : 1;  // overflow flag
     Byte N : 1;  // negative flag
 
-    void Reset(Mem &memory) {
+    void Reset(Mem& memory) {
         PC = 0xFFFC;                    // set program counter to fffc, reset
         SP = 0x00FF;                    // set stack pointer to 00ff , reset
         D = 0;                          // clear decimal flag
@@ -44,19 +49,31 @@ struct CPU {
         memory.Initialise();
     }
 
-    Byte FetchByte(u32 Cycles, Mem &memory) {
+    Byte FetchByte(u32 Cycles, Mem& memory) {
         Byte Data = memory[PC];
         PC++;
         Cycles--;
         return Data;
     }
 
-    void Execute(u32 Cycles, Mem &memory) {
+    static constexpr Byte INS_LDA_IM = 0xA9;
+
+    void Execute(u32 Cycles, Mem& memory) {
         while (Cycles--) {
             Byte Ins = FetchByte(
                 Cycles,
                 memory);  // fetch instruction with help of program counter
-            (void)Ins;
+            switch (Ins) {
+            case INS_LDA_IM: {
+                Byte Value = FetchByte(Cycles, memory);
+                A = Value;
+                Z = (A == 0);
+                N = (A & 0b10000000) > 0;
+            } break;
+            default: {
+                printf("instruction not handled %d", Ins);
+            } break;
+            }
         }
     }
 };
@@ -65,6 +82,10 @@ int main() {
     CPU cpu;
     Mem mem;
     cpu.Reset(mem);
+    // start - inline program
+    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFD] = 0x42;
+    // end - inline program
     cpu.Execute(2, mem);
     return 0;
 }
